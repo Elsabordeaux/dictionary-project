eb4f4273be75d618b3b114d2fc624dc575d54cce
Elsabordeaux <snowpaws85@gmail.com>
2024-11-05 09:54:24 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import logo from "./dictionary-icon-8.jpg";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="logo img-fluid" alt="dictionary-logo" />
+      </header>
+    </div>
+  );
+}
