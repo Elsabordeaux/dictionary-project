12d94a9e2627fb0658f8a412e8f618ad3f54e26d
Elsabordeaux <snowpaws85@gmail.com>
2024-11-06 16:03:31 +0000

    added API intergration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [word, setWord] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${word}`);
+
+    const apiKey = `dd83fab04t350832b43f1o8b985e5044`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleWordChange(event) {
